339ac43a6a35e61a1676ac017f5aede9abe6463f
Nils <nilsbaumgartner1994@gmail.com>
2021-09-20 20:18:06 +0200

    Run linters

--- src/ProfileHelper.ts
+++ src/ProfileHelper.ts
@@ -1,37 +1,43 @@
-import TokenHelper from "./TokenHelper";
-import TokenVerifier from "./TokenVerifier";
+import TokenHelper from './TokenHelper';
+import TokenVerifier from './TokenVerifier';
 
-type CallbackFunctionAnyReturn = (body: any, client_id: string, scope: string, query: any) => any;
+type CallbackFunctionAnyReturn = (
+  body: any,
+  client_id: string,
+  scope: string,
+  query: any
+) => any;
 
 export default class ProfileHelper {
+  static CUSTOM_GET_PROFILE_METHOD: CallbackFunctionAnyReturn;
 
-    static CUSTOM_GET_PROFILE_METHOD: CallbackFunctionAnyReturn;
-
-    static extractToken(req: any){
-        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === TokenVerifier.BEARER) {
-            return req.headers.authorization.split(' ')[1];
-        } else if (req.query && req.query.token) {
-            return req.query.token;
-        }
-        return null;
+  static extractToken(req: any) {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.split(' ')[0] === TokenVerifier.BEARER
+    ) {
+      return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
     }
+    return null;
+  }
 
-    static async getProfile(req: any, res: any, next: any){
-        console.log("getProfile");
-        console.log("req.headers: ",req.headers);
-        let token = ProfileHelper.extractToken(req);
-        console.log("token: ",token);
-        const payload = await TokenHelper.decodeJWT(token);
-        console.log("Payload: ",payload);
+  static async getProfile(req: any, res: any, next: any) {
+    console.log('getProfile');
+    console.log('req.headers: ', req.headers);
+    const token = ProfileHelper.extractToken(req);
+    console.log('token: ', token);
+    const payload = await TokenHelper.decodeJWT(token);
+    console.log('Payload: ', payload);
 
-        if(!!ProfileHelper.CUSTOM_GET_PROFILE_METHOD){
-            return ProfileHelper.CUSTOM_GET_PROFILE_METHOD(req, res, next, payload);
-        } else {
-            let email = payload.user.email;
+    if (ProfileHelper.CUSTOM_GET_PROFILE_METHOD) {
+      return ProfileHelper.CUSTOM_GET_PROFILE_METHOD(req, res, next, payload);
+    } else {
+      const email = payload.user.email;
 
-            //decodeJwtToken
-            return res.status(200).json({ email: email });
-        }
+      //decodeJwtToken
+      return res.status(200).json({email: email});
     }
-
+  }
 }
--- src/StorageHelper.ts
+++ src/StorageHelper.ts
@@ -1,104 +1,108 @@
-const uuidv4 = require("uuid/v4");
-const Hashids = require("hashids");
+const uuidv4 = require('uuid/v4');
+const Hashids = require('hashids');
 const hashids = new Hashids();
 
-
-const deHyphenatedUUID = () => uuidv4().replace(/-/gi, "");
+const deHyphenatedUUID = () => uuidv4().replace(/-/gi, '');
 const encodedId = () => hashids.encodeHex(deHyphenatedUUID());
 
 // A temporary cahce to store all the application that has login using the current session.
 // It can be useful for variuos audit purpose
-const sessionUser: { [id: string]: any} = {};
-const sessionApp: { [id: string]: any} = {};
+const sessionUser: {[id: string]: any} = {};
+const sessionApp: {[id: string]: any} = {};
 
 // these token are for the validation purpose
 const intrmTokenCache: {[id: string]: any[]} = {};
 
 export default class StorageHelper {
+  static originAppName: {[key: string]: string} = {
+    'http://192.168.178.35:8055': 'sso_consumer',
+    'http://192.168.178.35': 'simple_sso_consumer',
+  };
 
+  // app token to validate the request is coming from the authenticated server only.
+  static appTokenDB: {[appName: string]: string} = {
+    sso_consumer: 'l1Q7zkOL59cRqWBkQ12ZiGVW2DBL',
+    simple_sso_consumer: 'l1Q7zkOL59cRqWBkQ12ZiGVW2DBL',
+  };
 
-    static originAppName: {[key: string]: string} = {
-        "http://192.168.178.35:8055": "sso_consumer",
-        "http://192.168.178.35": "simple_sso_consumer"
-    };
+  static alloweOrigin: {[origin: string]: boolean} = {
+    'http://localhost': true,
+    '127.0.0.1': true,
+    'http://192.168.178.35:8055': true,
+    'http://192.168.178.35': true,
+  };
 
-    // app token to validate the request is coming from the authenticated server only.
-    static appTokenDB: {[appName: string]: string} = {
-        "sso_consumer": "l1Q7zkOL59cRqWBkQ12ZiGVW2DBL",
-        "simple_sso_consumer": "l1Q7zkOL59cRqWBkQ12ZiGVW2DBL"
-    };
+  static generateRandomToken(): string {
+    return encodedId();
+  }
 
-    static alloweOrigin: {[origin: string]: boolean} = {
-        "http://localhost": true,
-        "127.0.0.1": true,
-        "http://192.168.178.35:8055": true,
-        "http://192.168.178.35": true,
-    };
+  static storeUser(id: string, user: any) {
+    sessionUser[id] = user;
+  }
 
+  static getUser(id: string) {
+    return sessionUser[id];
+  }
 
-    static generateRandomToken(): string{
-        return encodedId();
+  static storeApplicationInCache(
+    origin: string,
+    id: string,
+    intrmToken: string
+  ) {
+    if (sessionApp[id] == null) {
+      sessionApp[id] = {
+        [StorageHelper.originAppName[origin]]: true,
+      };
+      StorageHelper.fillIntrmTokenCache(origin, id, intrmToken);
+    } else {
+      sessionApp[id][StorageHelper.originAppName[origin]] = true;
+      StorageHelper.fillIntrmTokenCache(origin, id, intrmToken);
     }
+  }
 
-    static storeUser(id: string, user: any){
-        sessionUser[id] = user;
-    }
+  static isSessionTokenAllowedForAppName(
+    globalSessionToken: string,
+    appName: string
+  ) {
+    return !!sessionApp[globalSessionToken][appName];
+  }
 
-    static getUser(id: string){
-        return sessionUser[id];
-    }
+  static isApptokenInTokeDB(appToken: string, appName: string) {
+    return appToken === StorageHelper.appTokenDB[appName];
+  }
 
-    static storeApplicationInCache(origin: string, id: string, intrmToken: string){
-        if (sessionApp[id] == null) {
-            sessionApp[id] = {
-                [StorageHelper.originAppName[origin]]: true
-            };
-            StorageHelper.fillIntrmTokenCache(origin, id, intrmToken);
-        } else {
-            sessionApp[id][StorageHelper.originAppName[origin]] = true;
-            StorageHelper.fillIntrmTokenCache(origin, id, intrmToken);
-        }
-    }
+  static isAuthorizedToGetPayload(ssoToken: string, appToken: string) {
+    const appName = StorageHelper.getAppnameFromIntrm(ssoToken);
+    const globalSessionToken =
+      StorageHelper.getGlobalSesionTokenFromIntrm(ssoToken);
 
-    static isSessionTokenAllowedForAppName(globalSessionToken: string, appName: string){
-        return !!sessionApp[globalSessionToken][appName]
-    }
+    return (
+      StorageHelper.isApptokenInTokeDB(appToken, appName) &&
+      StorageHelper.isSessionTokenAllowedForAppName(globalSessionToken, appName)
+    );
+  }
 
-    static isApptokenInTokeDB(appToken: string, appName: string){
-        return appToken === StorageHelper.appTokenDB[appName];
-    }
+  static fillIntrmTokenCache(origin: string, id: string, intrmToken: string) {
+    intrmTokenCache[intrmToken] = [id, StorageHelper.originAppName[origin]];
+  }
 
-    static isAuthorizedToGetPayload(ssoToken: string, appToken: string){
-        const appName = StorageHelper.getAppnameFromIntrm(ssoToken);
-        const globalSessionToken = StorageHelper.getGlobalSesionTokenFromIntrm(ssoToken);
+  static getContentFromIntrmCache(ssoToken: string) {
+    return intrmTokenCache[ssoToken];
+  }
 
-        return (StorageHelper.isApptokenInTokeDB(appToken,appName) &&
-            StorageHelper.isSessionTokenAllowedForAppName(globalSessionToken, appName));
-    }
+  static getGlobalSesionTokenFromIntrm(ssoToken: string) {
+    return StorageHelper.getContentFromIntrmCache(ssoToken)[0];
+  }
 
-    static fillIntrmTokenCache(origin: string, id: string, intrmToken: string) {
-        intrmTokenCache[intrmToken] = [id, StorageHelper.originAppName[origin]];
-    }
+  static getAppnameFromIntrm(ssoToken: string) {
+    return StorageHelper.getContentFromIntrmCache(ssoToken)[1];
+  }
 
-    static getContentFromIntrmCache(ssoToken: string){
-        return intrmTokenCache[ssoToken];
-    }
+  static deleteIntrmToken(ssoToken: string) {
+    delete intrmTokenCache[ssoToken];
+  }
 
-    static getGlobalSesionTokenFromIntrm(ssoToken: string){
-        return StorageHelper.getContentFromIntrmCache(ssoToken)[0]
-    }
-
-    static getAppnameFromIntrm(ssoToken: string){
-        return StorageHelper.getContentFromIntrmCache(ssoToken)[1]
-    }
-
-    static deleteIntrmToken(ssoToken: string){
-        delete intrmTokenCache[ssoToken];
-    }
-
-    static isAllowedOrigin(urlOrigin: string){
-        return StorageHelper.alloweOrigin[urlOrigin];
-    }
-
-
+  static isAllowedOrigin(urlOrigin: string) {
+    return StorageHelper.alloweOrigin[urlOrigin];
+  }
 }
