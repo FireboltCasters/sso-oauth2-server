7774c8c6b3b306cf97494ffdbe3d7f75918cdc30
Lint Action <lint-action@samuelmeuli.com>
2021-09-20 18:30:07 +0000

    Fix code style issues with Prettier

--- src/tests/TestTokenHelper.ts
+++ src/tests/TestTokenHelper.ts
@@ -2,16 +2,16 @@ import EnvironmentCredentials from '../EnvironmentCredentials';
 import TokenHelper from '../TokenHelper';
 
 test('Decode and Encode will give input', async () => {
-    EnvironmentCredentials.SYMETRIC_JWT_TOKEN = "Hallo";
-    const payload = {data: "Payload"}
-    let encoded = await TokenHelper.genJwtToken(payload);
-    let decoded = TokenHelper.decodeJWT(encoded);
-    expect(decoded.data).toBe(payload.data);
+  EnvironmentCredentials.SYMETRIC_JWT_TOKEN = 'Hallo';
+  const payload = {data: 'Payload'};
+  let encoded = await TokenHelper.genJwtToken(payload);
+  let decoded = TokenHelper.decodeJWT(encoded);
+  expect(decoded.data).toBe(payload.data);
 });
 
 test('Encode is not input', async () => {
-    EnvironmentCredentials.SYMETRIC_JWT_TOKEN = "Hallo";
-    const payload = {data: "Payload"}
-    let encoded = await TokenHelper.genJwtToken(payload);
-    expect(encoded).not.toBe(payload.data);
+  EnvironmentCredentials.SYMETRIC_JWT_TOKEN = 'Hallo';
+  const payload = {data: 'Payload'};
+  let encoded = await TokenHelper.genJwtToken(payload);
+  expect(encoded).not.toBe(payload.data);
 });
